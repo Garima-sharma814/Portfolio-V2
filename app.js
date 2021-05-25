00a5e020bdf7eedfd989b4328fdf5b809238987a
Garima <sharmagarima814@gmail.com>
2021-05-25 09:42:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const theme = document.querySelector("#theme-link");
+const btn = document.querySelector("#checkbox");
+
+// For theme 
+const btnValue = localStorage.getItem("themeBtn");
+
+if (btnValue === null || btnValue === "null") {
+	localStorage.setItem("themeBtn", btn.checked);
+	applyTheme(localStorage.getItem("themeBtn"));
+}
+
+if (btnValue === "true" || btnValue === "false") {
+	applyTheme(btnValue);
+}
+
+btn.addEventListener("change", () => {
+	localStorage.setItem("themeBtn", btn.checked);
+	applyTheme(localStorage.getItem("themeBtn"));
+});
+
+function applyTheme(value) {
+	if (value === "true") {
+		theme.href = "/css/light-theme.css";
+		btn.checked = true;
+	} else if (value === "false") {
+		theme.href = "/css/style.css";
+		btn.checked = false;
+	}
+}
