ecf21b08e0ccf01e17043dbe9480ee98bb957d51
Garima <sharmagarima814@gmail.com>
2021-08-05 15:31:05 +0530

    remove: theme change code

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-const theme = document.querySelector("#theme-link");
-const btn = document.querySelector("#checkbox");
-const card = document.querySelector("#card");
-const pop = document.querySelector("#pop");
-
-// For theme 
-const btnValue = localStorage.getItem("themeBtn");
-
-if (btnValue === null || btnValue === "null") {
-	localStorage.setItem("themeBtn", btn.checked);
-	applyTheme(localStorage.getItem("themeBtn"));
-}
-
-if (btnValue === "true" || btnValue === "false") {
-	applyTheme(btnValue);
-}
-
-btn.addEventListener("change", () => {
-	localStorage.setItem("themeBtn", btn.checked);
-	applyTheme(localStorage.getItem("themeBtn"));
-});
-
-function applyTheme(value) {
-	if (value === "true") {
-		theme.href = "/css/light-theme.css";
-		btn.checked = true;
-	} else if (value === "false") {
-		theme.href = "/css/style.css";
-		btn.checked = false;
-	}
-};
-
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
 	myFunction()
